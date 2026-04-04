2491fd5992b9823a1f36e4a0e1b35026e34b48dc
Puneet Sharma <puneet.aery43@gmail.com>
2026-04-04 11:14:08 +0530

    first commit

---
+++ learningpython/1_first.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+print("Welcome to Python programming.")
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("hey how are you.")
+engine.runAndWait()
+print(''' jj
+      jj
+      jj''')
+print(""" jjjj
+      jjj
+      jjj
+      """)
+ 
---
+++ learningpython/2_identifier.py
@@ -0,0 +1,5 @@
+# name given to a memory location is called an identifier. It is used to identify a variable, function, class, module or other object in Python. An identifier starts with a letter (A-Z or a-z) or an underscore (_) followed by zero or more letters, underscores and digits (0-9). Python does not allow special characters like @, $, and % within identifiers.
+a=1 # a is an integer variable
+b=2.2 # b is a float variable
+c="Hello" # c is a string variable
+d=True # d is a boolean variable
