9db87173b5cc8545cee85e929c9635ccdbf9c6dd
Puneet Sharma <puneet.aery43@gmail.com>
2026-04-12 15:18:30 +0530

    upto loops

---
+++ learningpython/17_loops.py
@@ -0,0 +1,38 @@
+# # loops are used to repeat a block of code multiple times. In Python, there are two main types of loops: for loops and while loops.
+# while loop is used to execute a block of code as long as a specified condition is true. The syntax for a while loop is:
+# for loop is used to iterate over a sequence (such as a list, tuple, or string) or other iterable object. The syntax for a for loop is:
+
+
+# i = 1
+# while i<=10:
+#     print(i)
+#     i = i+1 
+#     # or i += 1
+
+# l1 = [1,2,3,4,7,"hello"]
+# for i in l1:
+#     print(i)    
+    
+# l = [1,2,3,4,7,9]
+# i = 0
+# while i < len(l):
+#     print(l[i])
+#     i += 1
+
+    # range function is used to generate a sequence of numbers. It can take one, two, or three arguments. The syntax for the range function is:
+# for i in range(1,11):
+#         print(i)
+
+# for loop with range function
+# l = [1,2,3,4,7,9]
+# for i in range(len(l)):
+#     print(l[i])   
+
+
+
+# for loop with else statement
+
+l = [1,2,3,4,7,9]
+for i in l:
+    print(i)
+else:    print("loop is completed") 
--- learningpython/5_practiceofifelse.py
+++ learningpython/5_practiceofifelse.py
@@ -1,11 +1,21 @@
 
-sub1 = int(input("Enter marks for subject 1: "))
-sub2 = int(input("Enter marks for subject 2: "))    
-sub3 = int(input("Enter marks for subject 3: "))
+# sub1 = int(input("Enter marks for subject 1: "))
+# sub2 = int(input("Enter marks for subject 2: "))    
+# sub3 = int(input("Enter marks for subject 3: "))
 
-percentage = ((sub1+sub2+sub3)/300)*100
-if (percentage>=40 and sub1>=(33/100)*100 and sub2>=(33/100)*100 and sub3>=(33/100)*100):
-    print("Congratulations! You have passed the exam.")
-else:
-    print("Sorry! You have failed the exam.")   
+# percentage = ((sub1+sub2+sub3)/300)*100
+# if (percentage>=40 and sub1>=33 and sub2>=33 and sub3>=33):
+#     print("Congratulations! You have passed the exam.")
+# else:
+#     print("Sorry! You have failed the exam.")   
     
+    
+post = input("enter your post: ")
+
+if("Puneet".lower() in post.lower()):
+    
+# or if("Puneet".lower() == post.lower()):
+
+    print("this post is talking about puneet.")
+else:
+    print("this post is saying something else")
